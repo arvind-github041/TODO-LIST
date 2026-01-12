306e8b45e434ec2edba9b2355a0dae38c03bb7bb
arvind-github041 <arvindpersonal18@gmail.com>
2026-01-12 23:44:39 +0530

    final commit

--- TO-DO.js
+++ TO-DO.js
@@ -10,9 +10,9 @@ function gettodo(){
         dueDate : elementduedate
  });
  rander();
- console.log(data);   
+ console.log(data);
 
- document.querySelector('.js-todo-element').value =''; 
+ document.querySelector('.js-todo-element').value ='';
  document.querySelector('.js-dueDate').value ='';
 }
 function rander() {
@@ -22,9 +22,10 @@ function rander() {
         innercode += `
             <div class="todo-item ${data[i].done ? 'done' : ''}">
                 <span class="tick">✔</span>
-                <span>${data[i].name} — ${data[i].dueDate}</span>
+                <span>${i+1}\] ${data[i].name} — ${data[i].dueDate}</span>
                 <button onclick="markDone(${i})">Done</button>
                 <button onclick="deleteTodo(${i})">Delete</button>
+                </br>
             </div>
         `;
     }
@@ -41,3 +42,11 @@ function deleteTodo(index) {
     data.splice(index, 1);
     rander();
 }
+document.querySelectorAll('.js-todo-element, .js-dueDate')
+  .forEach((input) => {
+    input.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        gettodo();
+      }
+    });
+  });
